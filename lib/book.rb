c2c57408395b16a9e015c6f816a3affc2f77d3d7
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-09 08:09:40 +0300

    Completed assignmenr

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,7 @@
 class Book
+    # add attributes
     attr_accessor :title, :author, :page_count, :genre
+    # initialize with a title
     def initialize(title)
         @title = title
     end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,20 @@
-# Make your shoe class here!
+# require 'pry'
 class Shoe
+    # add attributes
     attr_accessor :brand, :color, :size, :material, :condition
+    # initialize with a brand
     def initialize(brand)
         @brand = brand
     end
     
     def cobble
         puts "Your shoe is as good as new!"
+        @condition = "new"
     end        
-end    
+end  
+
+# shoe = Shoe.new
+# shoe.condition = "new" 
+# puts shoe.condition
+# shoe.cobble
+# binding.pry
